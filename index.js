456b43a57c20d1b1f541d68ea141b70539483203
tauqeerabbas <tauqeerabbas334@gmail.com>
2025-02-06 00:06:59 +0500

    Built edit filename feature.

--- index.js
+++ index.js
@@ -21,6 +21,16 @@ app.get('/file/:filename', (req, res) => {
     })
 })
 
+app.get('/edit/:filename', (req, res) => {
+    res.render('edit', {filename: req.params.filename});
+})
+
+app.post('/edit', (req, res) => {
+    fs.rename(`./files/${req.body.previous}`, `./files/${req.body.new}`, (err) => {
+        res.redirect("/");
+    });
+});
+
 app.post('/create', (req, res) => {
     fs.writeFile(`./files/${req.body.title.split(' ').join('')}.txt`, req.body.description, (err) => {
         res.redirect("/");
