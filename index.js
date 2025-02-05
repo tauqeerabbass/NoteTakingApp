3da044b3e17dd1bc28d152dbf3c900b2a2346b67
tauqeerabbas <tauqeerabbas334@gmail.com>
2025-02-05 23:32:47 +0500

    Done with the display of the notes made by the user on a new page.

--- index.js
+++ index.js
@@ -15,6 +15,12 @@ app.get('/', (req, res) => {
     })
 });
 
+app.get('/file/:filename', (req, res) => {
+    fs.readFile(`./files/${req.params.filename}`, "utf-8", (err, data) => {
+        res.render("show", {filename: req.params.filename, data: data});
+    })
+})
+
 app.post('/create', (req, res) => {
     fs.writeFile(`./files/${req.body.title.split(' ').join('')}.txt`, req.body.description, (err) => {
         res.redirect("/");
